107e865a7fffc72b796e535a7e9a7444c50b7878
Beiobi <a.simanovskij@gmail.com>
2021-02-23 13:15:13 +0300

    feat: add delete btn and likes check

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,17 @@
+import {CurrentUserContext} from '../contexts/CurrentUserContext';
+import React from 'react';
+
+
 function Card(props) {
+
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = props.owner._id === currentUser._id;
+
+  const cardDeleteButtonClassName = isOwn ? `button button_type_delete` : `button_hidden`;
+
+  const isLiked = props.likes.some(i => i._id === currentUser._id);
+  const cardLikeButtonClassName = isLiked ? `button button_type_like-black` : `button button_type_like`;
 
   function handleCardClick() {
     props.onCardClick(props.name, props.link);
@@ -7,12 +20,13 @@ function Card(props) {
   return (
     <article className="card">
       <img className="card__image" src={props.link} alt={props.name} onClick={handleCardClick} />
-      <button className="button button_type_delete" aria-label="Удалить" type="button" />
+      <button className={cardDeleteButtonClassName} aria-label="Удалить" type="button" />
       <div className="card__description">
         <h2 className="card__name">{props.name}</h2>
         <div className="card__likes">
-          <button className="button button_type_like" aria-label="Лайк" type="button" />
+          <button className={cardLikeButtonClassName} aria-label="Лайк" type="button" />
           <span className="card__like-counter">{props.likes.length}</span>
+          {console.log(isOwn)}
         </div>
       </div>
     </article>
